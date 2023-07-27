c36673caddd22a82417637de7bfdf705a93d0679
Jihun Park <jihunpark.tech@gmail.com>
2023-07-27 23:40:08 +0900

    [toby spring boot] Hello 컨트롤러 매핑과 바인딩

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -19,6 +19,8 @@ public class HellobootApplication {
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			HelloController helloController = new HelloController();
+
 			servletContext.addServlet("frontController", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -26,12 +28,14 @@ public class HellobootApplication {
 					if (req.getRequestURI().equals("/servlet/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 						String name = req.getParameter("name");
 
+						String ret = helloController.hello(name);
+
 						resp.setStatus(HttpStatus.OK.value());
 						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println("hello " + name);
+						resp.getWriter().println(ret);
 					}
 					else if (req.getRequestURI().equals("/user")) {
-
+						// ...
 					}
 				 	else {
 						resp.setStatus(HttpStatus.NOT_FOUND.value());
