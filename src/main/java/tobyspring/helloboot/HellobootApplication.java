d9ab47dc427a44b9bdd8d0b333eafdeafe0b7e17
Jihun Park <jihunpark.tech@gmail.com>
2023-07-29 12:11:07 +0900

    [toby spring boot] DispatcherServlet으로 전환

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,47 +3,23 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		GenericApplicationContext applicationContext = new GenericApplicationContext(); // 스프링 컨테이너 생성
-		applicationContext.registerBean(HelloController.class); // 빈 오브젝트 클래스 정보 등록
-		applicationContext.registerBean(SimpleHelloService.class);
-		applicationContext.refresh(); // 구성 정보로 컨테이너 초기화(빈 오브젝트를 직접 생성)
+    public static void main(String[] args) {
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(); // 스프링 컨테이너 생성
+        applicationContext.registerBean(HelloController.class); // 빈 오브젝트 클래스 정보 등록
+        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.refresh(); // 구성 정보로 컨테이너 초기화(빈 오브젝트를 직접 생성)
 
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("frontController", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					// 인증, 보안, 다국어, 공통 기능 ..
-					if (req.getRequestURI().equals("/servlet/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						String name = req.getParameter("name");
-
-						HelloController helloController = applicationContext.getBean(HelloController.class); // 컨테이너가 관리하는 빈 오브젝트 획득
-						String ret = helloController.hello(name);
-
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					}
-				 	else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-
-				}
-			}).addMapping("/*");
-		});
-		webServer.start();
-	}
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("dispatcherServlet",
+                    new DispatcherServlet(applicationContext)
+            ).addMapping("/*");
+        });
+        webServer.start();
+    }
 }
