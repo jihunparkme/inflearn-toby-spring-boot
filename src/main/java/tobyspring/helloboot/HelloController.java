8273366cacf071605170f9607f6665dfe5a9c8c7
Jihun Park <jihunpark.tech@gmail.com>
2023-07-29 11:58:52 +0900

    [toby spring boot] 의존 오브젝트 DI 적용 주석 추가

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,8 +3,12 @@ package tobyspring.helloboot;
 import java.util.Objects;
 
 public class HelloController {
+    // 주입 받은 오브젝트는 내부 멤버 필드로 저장해두고 이용
     private final HelloService helloService;
 
+    /**
+     * 의존 오브젝트를 생성자를 통해서 DI 어셈블러인 컨테이너가 주입을 할 수 있게 생성자 파라미터를 정의
+     */
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
     }
