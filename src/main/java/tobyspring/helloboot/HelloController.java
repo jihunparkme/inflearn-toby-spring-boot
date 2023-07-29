f0e0620d958e25814d8aff637377f2ff2714b6b4
Jihun Park <jihunpark.tech@gmail.com>
2023-07-29 13:18:49 +0900

    [toby spring boot] 애노테이션 매핑 정보 사용

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,15 @@
 package tobyspring.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+/**
+ * 클래스 레벨의 @RequestMapping 과 메소드 레벨의 @GetMapping 두 가지의 정보를 조합해서 매핑에 사용할 최종 정보 생성
+ */
+@RequestMapping("/hello")
 public class HelloController {
     // 주입 받은 오브젝트는 내부 멤버 필드로 저장해두고 이용
     private final HelloService helloService;
@@ -13,6 +21,11 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    /**
+     * 컨트롤러 메소드의 리턴값을 웹 요청의 바디에 적용하도록 @ResponseBody 선언
+     */
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -9,13 +9,20 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class HellobootApplication {
 
     public static void main(String[] args) {
+        /**
+         * Spring Container 생성
+         */
         GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(); // 스프링 컨테이너 생성
         applicationContext.registerBean(HelloController.class); // 빈 오브젝트 클래스 정보 등록
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh(); // 구성 정보로 컨테이너 초기화(빈 오브젝트를 직접 생성)
 
+        /**
+         * Servlet Container 를 실행하면서 Servlet 등록
+         */
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            // DispatcherServlet 은 스프링 컨테이너에 등록된 빈 클래스에 있는 매핑 애노테이션 정보를 참고해서 웹 요청을 전달할 오브젝트와 메소드를 선정
             servletContext.addServlet("dispatcherServlet",
                     new DispatcherServlet(applicationContext)
             ).addMapping("/*");
