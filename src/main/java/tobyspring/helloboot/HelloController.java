f3dc7b3a6678ee6ed9dd037f17fd80ea74787d8f
Jihun Park <jihunpark.tech@gmail.com>
2023-07-30 22:29:49 +0900

    [toby spring boot] @Component scan

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,15 +1,14 @@
 package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 /**
  * 클래스 레벨의 @RequestMapping 과 메소드 레벨의 @GetMapping 두 가지의 정보를 조합해서 매핑에 사용할 최종 정보 생성
  */
-@RequestMapping("/hello")
+@RestController
 public class HelloController {
     // 주입 받은 오브젝트는 내부 멤버 필드로 저장해두고 이용
     private final HelloService helloService;
@@ -24,8 +23,7 @@ public class HelloController {
     /**
      * 컨트롤러 메소드의 리턴값을 웹 요청의 바디에 적용하도록 @ResponseBody 선언
      */
-    @GetMapping
-    @ResponseBody
+    @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
