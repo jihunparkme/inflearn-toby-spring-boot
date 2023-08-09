042ece85949d5e1cecdc6a30458ba9482b73fee2
Jihun Park <jihunpark.tech@gmail.com>
2023-08-09 22:57:51 +0900

    [toby spring boot] 프로퍼티 빈의 후처리기 도입

--- src/main/java/tobyspring/config/autoconfig/ServerProperties.java
+++ src/main/java/tobyspring/config/autoconfig/ServerProperties.java
@@ -2,9 +2,11 @@ package tobyspring.config.autoconfig;
 
 import lombok.Getter;
 import lombok.Setter;
+import tobyspring.config.MyConfigurationProperties;
 
 @Getter
 @Setter
+@MyConfigurationProperties(prefix = "server")
 public class ServerProperties {
     private String contextPath;
     private int port;
