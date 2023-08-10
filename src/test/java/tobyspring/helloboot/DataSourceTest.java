a453c0c6bcdab04ed77995859e81b62e1c44b1a5
Jihun Park <jihunpark.tech@gmail.com>
2023-08-10 10:04:51 +0900

    [toby spring boot] DataSource 자동 구성 클래스
    주석 추가

--- src/test/java/tobyspring/helloboot/DataSourceTest.java
+++ src/test/java/tobyspring/helloboot/DataSourceTest.java
@@ -11,6 +11,12 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * application.properties 파일 등록은 스프링 프레임워크 기본 동작 방식은 아니고 스프링 부트 초기화 과정에서 추가해주는 것.
+ * 자동으로 포함이 안 되므로
+ * @TestPropertySource("classpath:/application.properties")
+ * 로 properties 정보를 읽어오도록 설정
+ */
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = HellobootApplication.class)
 @TestPropertySource("classpath:/application.properties")
