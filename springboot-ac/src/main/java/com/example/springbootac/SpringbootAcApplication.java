1385c6189a3d61addd6a66384916f2d0a7eed720
Jihun Park <jihunpark.tech@gmail.com>
2023-08-12 10:46:33 +0900

    [toby spring boot] 자동 구성 조건 결과 확인

--- springboot-ac/src/main/java/com/example/springbootac/SpringbootAcApplication.java
+++ springboot-ac/src/main/java/com/example/springbootac/SpringbootAcApplication.java
@@ -1,10 +1,34 @@
 package com.example.springbootac;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionEvaluationReport;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootAcApplication {
+    /**
+     * 조건이 매칭된 자동 구성 클래스와 메소드를 출력
+     */
+    @Bean
+    ApplicationRunner run(ConditionEvaluationReport report) {
+        return args -> {
+            long result = report.getConditionAndOutcomesBySource().entrySet().stream()
+                    .filter(co -> co.getValue().isFullMatch()) // 컨디션 조건을 모두 통과한 빈 목록
+                    .filter(co -> co.getKey().indexOf("Jmx") < 0) // Jmx 관련 구성 정보 제외
+                    .map(co -> {
+                        System.out.println(co.getKey());
+                        co.getValue().forEach(c -> {
+                            System.out.println("\t" + c.getOutcome()); // 컨디셔널 통과 조건
+                        });
+                        System.out.println();
+                        return co;
+                    }).count();
+
+            System.out.println(result);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootAcApplication.class, args);
